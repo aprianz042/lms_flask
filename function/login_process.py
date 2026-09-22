16663a9eadc3a24a370fb24547924f6f2c652ea6
Ari Apriansyah <77948753+aprianz042@users.noreply.github.com>
2026-09-22 14:26:46 +0700

    Update login_process.py

--- function/login_process.py
+++ function/login_process.py
@@ -3,59 +3,47 @@ from function.koneksi import get_db_connection
 def proses_login(id_user, level):
     connection = get_db_connection()
     with connection.cursor() as cursor:
-        if level == 'admin' or level == 'operator':
-            cursor.execute('SELECT * FROM user WHERE id = %s', (id_user,))
+        if level == '' or level == '':
+            cursor.execute('SELECT * FROM  WHERE  = %s', (id_user,))
             user = cursor.fetchone()
             if user:
                 connection.close()
-                return user['id'], user['nama'], user['no_id'], user['level']
+                return user[''], user[''], user[''], user['']
         
-        if level == 'dosen' or level == 'pelatih':
-            cursor.execute('SELECT * FROM pengajar WHERE id_pengajar = %s', (id_user,))
+        if level == '' or level == '':
+            cursor.execute('SELECT * FROM  WHERE  = %s', (id_user,))
             pengajar = cursor.fetchone()         
             if pengajar:
                 connection.close()
-                return pengajar['id_pengajar'], pengajar['nama_pengajar'], pengajar['nip'], pengajar['kategori']
+                return pengajar[''], pengajar[''], pengajar[''], pengajar['']
         
-        if level == 'mahasiswa':
-            cursor.execute('SELECT * FROM mahasiswa WHERE id_mahasiswa = %s', (id_user,))
+        if level == '':
+            cursor.execute('SELECT * FROM  WHERE  = %s', (id_user,))
             mhs = cursor.fetchone()                
             if mhs:
                 connection.close()
-                return mhs['id_mahasiswa'], mhs['nama_mahasiswa'], mhs['nim'], 'mahasiswa'
+                return mhs[''], mhs[''], mhs[''], ''
     return None
 
 def get_session_user(username, password_hash):
     connection = get_db_connection()
     with connection.cursor() as cursor:
-        cursor.execute('SELECT * FROM user WHERE no_id = %s AND pass = %s', (username, password_hash))
+        cursor.execute('SELECT * FROM  WHERE  = %s AND  = %s', (username, password_hash))
         user = cursor.fetchone()
         if user:
             connection.close()
-            user_ = user['id']
-            no_id_ = user['no_id']
-            nama_ = user['nama']
-            level_ = user['level']
             return user_, no_id_, nama_, level_
         
-        cursor.execute('SELECT * FROM pengajar WHERE nip = %s AND password = %s', (username, password_hash))
+        cursor.execute('SELECT * FROM  WHERE  = %s AND  = %s', (username, password_hash))
         pengajar = cursor.fetchone()
         if pengajar:
             connection.close()
-            user_ = pengajar['id_pengajar'] 
-            no_id_ = pengajar['nip']  
-            nama_ = pengajar['nama_pengajar']
-            level_ = pengajar['kategori'] 
             return user_, no_id_, nama_, level_
 
-        cursor.execute('SELECT * FROM mahasiswa WHERE nim = %s AND password = %s', (username, password_hash))
+        cursor.execute('SELECT * FROM  WHERE  = %s AND  = %s', (username, password_hash))
         mhs = cursor.fetchone()
         if mhs:
             connection.close()
-            user_ = mhs['id_mahasiswa']
-            no_id_ = mhs['nim']  
-            nama_ = mhs['nama_mahasiswa'] 
-            level_ = "mahasiswa" 
             return user_, no_id_, nama_, level_
         else:
             connection.close()
